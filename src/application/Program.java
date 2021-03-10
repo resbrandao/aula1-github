49cd0484a37ff1efd73a9f5cf2d1f781dfd6002c
Rejane Brandao <resbrandao@gmail.com>
2021-03-10 20:58:14 -0300

    Acrescentada msg de bom dia e eliminado espacos

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Boa Noite!!!");
 		System.out.println("Boa Tarde!!!");
+		System.out.println("Bom dia!!!");
 	}
 
 }
